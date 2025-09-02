6a18f7b800fe37ca4b3d4de1288a5f6a929dbdb7
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-09-02 20:47:53 +0500

    add pics

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+    <img  src={assets.logo} alt="" className="logo" />
+    <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile-app</li>
+        <li>Contact us</li>
+    </ul>
+    <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+            
+        </div>
+    </div>
     </div>
   )
 }
