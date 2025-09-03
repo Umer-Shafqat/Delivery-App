8c711a661c72160e0cca8f44f9788dc61aa6007c
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-09-03 11:59:59 +0500

    add Header Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Cart from './pages/Cart/Cart'
 
 function App() {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+
+        <Route path="/" 
+        element={<Home/>}/>
+
+      <Route path="/cart"
+      element={<Cart/>}/>
+
+      <Route path="/placeOrder" 
+        element={<PlaceOrder/>}/>
+        
+      </Routes>
     </div>
   )
 }
--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./PlaceOrder.css"
+
 const PlaceOrder = () => {
   return (
     <div>
