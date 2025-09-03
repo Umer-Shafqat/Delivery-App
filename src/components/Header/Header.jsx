c194a503a82c4529d100c4a03d52fc94ea789681
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-09-03 12:56:38 +0500

    Change in pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,14 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='header' >
+      <div className='header-contents'>
+        <h2>Order your favourite food here</h2>
+        <p>🍴 Discover fresh, hygienic, and high-quality meals at your fingertips.
+           ✅ Track food standards with real-time quality checks and ratings.
+           📊 Enjoy transparency and trust in every bite you order.</p>
+           <button>View Menu</button>
+      </div>
     </div>
   )
 }
