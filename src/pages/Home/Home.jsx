bea71a0a96d51079c3ece6384a62ce7de642a79f
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-09-08 19:26:01 +0500

    add storecontext.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
 import Header from '../../components/Header/Header'
 import Exploremenu from '../../components/Exploremenu/Exploremenu'
 
 const Home = () => {
+
+  const[category,setCategory]=useState("All");
+
   return (
     <div>
       <Header/> 
-      <Exploremenu/>
+      <Exploremenu category={category} setCategory={setCategory}/>
         </div>
   )
 }
