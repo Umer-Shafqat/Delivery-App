e3e6ed52eeb1420ed7c4a97e763a30f9ea0556bc
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-09-09 13:41:26 +0500

    modify fooditem and food item files

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import './FoodItem.css'
 import {assets} from '../../assets/assets'
 
-const FoodItem = ({id,name,price,description,image,rating}) => {
+const FoodItem = ({id,name,price,description,image}) => {
+  
+  const [itemCount, setItemCount] = useState(0);
   return (
     <div className='food-item'> 
       <div className="food-item-img-container">
         <img className='food-item-image' src={image} alt={name} />
+        {!itemCount
+          ?<img className="add" src={assets.add_icon_white} alt="" />
+          : <div className="food_item">
+
+          </div>
+        }
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
