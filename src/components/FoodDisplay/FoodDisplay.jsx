eaad59fc99d0b888556316559d7809e52c0e82c7
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-09-09 13:00:31 +0500

    add fooditem jsx file

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
 import './FoodDisplay.css'
-import { StoreContext } from '../../context/Storecontext'
+import { StoreContext } from '../../context/StoreContext'
+import React, { useContext } from 'react'
+import FoodItem from '../FoodItem/FoodItem'
+
+
 const FoodDisplay = ({ category }) => {
 
   const {food_list} = useContext(StoreContext)
 
   return (
-    <div className='food-display' id='food-display'> 
-      {food_list.map(food => (
-        <div key={food._id} className="food-item">
-          <img src={food.image} alt={food.name} />
-          <h3>{food.name}</h3>
-          <p>{food.description}</p>
-          <span>${food.price}</span>
-        </div>
-      ))}
-    </div>
+    <div className='food-display' id="food-display">
+      <h2>Top Dishes near you</h2>
+      <div className="food-display-list">
+           {food_list.map((item,index) => {
+            return <FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image} />
+           })}
+      </div>
+      </div>
   )
 }
-
 export default FoodDisplay
 
