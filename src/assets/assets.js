ff06ceb4f438864237ac8e5de754a4c27464cf84
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-09-09 21:17:12 +0500

    chaneges in food_list

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -93,7 +93,7 @@ export const menu_list = [
         menu_image : menu_3
     },
     {
-        menu_name : "Sandwish",
+        menu_name : "Sandwich",
         menu_image : menu_4
     },
      {
@@ -149,222 +149,221 @@ export const food_list = [
     },
         {
         _id:"5",
-        name : "Chicken Salad",
+        name : "Lasagna Rolls",
         image : food_5,
         price: 14.99,
-        description: "A classic Chicken salad with romaine lettuce, croutons, and Parmesan cheese.",
-        category: "salad"
+        description: "Creamy ricotta and spinach rolled in lasagna noodles, topped with rich sauce and melted cheese. A fun, individual twist on classic lasagna.",
+        category: "Rolls"
     },
     {
         _id:"6",
-        name : "Chicory Salad",
+        name : "Chicken Tikka Rolls",
         image : food_6,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Soft tortillas filled with juicy chicken tikka, fresh onions, and herbs, wrapped to perfection. A flavorful grab-and-go twist on classic Indian street food.",
+        category: "Rolls"
     },
       {
         _id:"7",
-        name : "Chicory Salad",
+        name : "Chicken Kathi Rolls",
         image : food_7,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "A flaky paratha wrap filled with spiced chicken, sautéed veggies, and tangy sauces. A delicious, handheld street-food favorite.",
+        category: "Rolls"
     },
        {
         _id:"8",
-        name : "Chicory Salad",
+        name : "calzone Rolls",
         image : food_8,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "This is a calzone, an Italian folded pizza. It’s made with pizza dough stuffed with cheese, sauce, and fillings like meat or vegetables, then baked until golden and topped with sesame seeds.",
+        category: "Rolls"
     },
         {
         _id:"9",
-        name : "Chicory Salad",
+        name : "raspberry ice cream",
         image : food_9,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "This is raspberry ice cream. A creamy frozen dessert blended with fresh raspberries, giving it a smooth texture with bursts of fruity flavor.",
+        category: "Dessert"
     },
          {
         _id:"10",
-        name : "Chicory Salad",
+        name : "Apple Mille-Feuille with Sorbet",
         image : food_10,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Layers of flaky pastry filled with creamy apple compote and served with a refreshing sorbet.",
+        category: "Dessert"
     },
          {
         _id:"11",
-        name : "Chicory Salad",
+        name : "Strawberry Panna Cotta",
         image : food_11,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "A creamy dessert made with fresh strawberries and a hint of vanilla, set in a delicate gelatin.",
+        category: "Dessert"
     },
          {
         _id:"12",
-        name : "Chicory Salad",
+        name : "Vanilla Ice Cream in Sprinkle Bowl",
         image : food_12,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
-    },  
+        description: "A classic vanilla ice cream served in a bowl with colorful sprinkles on top.",
+        category: "Dessert"
+    },
       {
         _id:"13",
-        name : "Chicory Salad",
+        name : "Classic Club Sandwich",
         image : food_13,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
-    }, 
+        description: "Triple-layered toasted bread stacked with turkey, bacon, lettuce, tomato, and mayo for a hearty bite.",
+        category: "Sandwich"
+    },
          {
         _id:"14",
-        name : "Chicory Salad",
+        name : "Caprese Sandwich",
         image : food_14,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
-    }, 
+        description: "Fresh mozzarella, tomatoes, and basil drizzled with balsamic glaze on toasted bread.",
+        category: "Sandwich"
+    },
          {
         _id:"15",
-        name : "Chicory Salad",
+        name : "Grilled Cheese Melt",
         image : food_15,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
-    }, 
+        description: "Toasted bread filled with a blend of melted cheeses, served with a side of marinara sauce for dipping.",
+        category: "Sandwich"
+    },
          {
         _id:"16",
-        name : "Chicory Salad",
+        name : "Chicken Pesto Panini",
         image : food_16,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Grilled chicken, basil pesto, and melted cheese pressed between ciabatta bread.",
+        category: "Sandwich"
     },  {
         _id:"17",
-        name : "Chicory Salad",
+        name : "Red Velvet Cake",
         image : food_17,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Soft crimson sponge with a hint of cocoa, paired with smooth cream cheese frosting.",
+        category: "Cake"
     }, 
          {
         _id:"18",
-        name : "Chicory Salad",
+        name : "Lemon Drizzle Cake",
         image : food_18,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "A light and zesty cake infused with fresh lemon juice and zest, topped with a sweet lemon glaze.",
+        category: "Cake"
     }, 
          {
         _id:"19",
-        name : "Chicory Salad",
+        name : "Black Forest Cake",
         image : food_19,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Layers of chocolate sponge cake, cherries, and whipped cream come together in this classic dessert.",
+        category: "Cake"
     }, 
          {
         _id:"20",
-        name : "Chicory Salad",
+        name : "Red Velvet Cake",
         image : food_20,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Soft crimson sponge with a hint of cocoa, paired with smooth cream cheese frosting.",
+        category: "Cake"
     },  {
         _id:"21",
-        name : "Chicory Salad",
+        name : "Vegetable Fried Rice",
         image : food_21,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "A flavorful mix of stir-fried vegetables and rice, seasoned with soy sauce and sesame oil.",
+        category: "Rice"
     }, 
          {
         _id:"22",
-        name : "Chicory Salad",
+        name : "Paella Valenciana",
         image : food_22,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "A traditional Spanish rice dish made with saffron-infused rice, a variety of seafood, and vegetables.",
+        category: "Rice"
     }, 
          {
         _id:"23",
-        name : "Chicory Salad",
+        name : "Mango Sticky Rice",
         image : food_23,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Sweet sticky rice topped with ripe mango slices and drizzled with coconut milk.",
+        category: "Rice"
     }, 
          {
         _id:"24",
-        name : "Chicory Salad",
+        name : "Chicken Biryani",
         image : food_24,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Aromatic basmati rice cooked with tender chicken pieces, flavored with spices and herbs.",
+        category: "Rice"
     },  {
         _id:"25",
-        name : "Chicory Salad",
+        name : "Classic French Fries",
         image : food_25,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Crispy golden fries served with a side of tangy ketchup.",
+        category: "Fries"
     }, 
          {
         _id:"26",
-        name : "Chicory Salad",
+        name : "Curly Fries",
         image : food_26,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Crispy curly fries seasoned to perfection, served with a side of zesty dipping sauce.",
+        category: "Fries"
     }, 
          {
         _id:"27",
-        name : "Chicory Salad",
+        name : "Sweet Potato Fries",
         image : food_27,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
-    }, 
-         {
+        description: "Crispy sweet potato fries served with a side of tangy ketchup.",
+        category: "Fries"
+    },  {
         _id:"28",
-        name : "Chicory Salad",
+        name : "Cheese Fries",
         image : food_28,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Crispy fries topped with melted cheese and served with a side of ranch dressing.",
+        category: "Fries"
     },  {
         _id:"29",
-        name : "Chicory Salad",
+        name : "Chow Mein",
         image : food_29,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Stir-fried noodles with a mix of vegetables and your choice of protein, seasoned with soy sauce.",
+        category: "Chinese"
     }, 
          {
         _id:"30",
-        name : "Chicory Salad",
+        name : "Sweet and Sour Pork",
         image : food_30,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Tender pork pieces stir-fried with bell peppers and pineapple in a tangy sweet and sour sauce.",
+        category: "Chinese"
     },  {
         _id:"31",
-        name : "Chicory Salad",
+        name : "Kung Pao Chicken",
         image : food_31,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Spicy stir-fried chicken with peanuts, vegetables, and chili peppers.",
+        category: "Chinese"
     }, 
          {
         _id:"32",
-        name : "Chicory Salad",
+        name : "Mapo Tofu",
         image : food_32,
         price: 21.23,
-        description: "Crisp red endive leaves paired with creamy blue cheese, crunchy walnuts, and fresh greens. A perfect balance of bitter, savory, and nutty flavors for a light yet elegant bite.",
-        category: "salad"
+        description: "Spicy and numbing tofu dish cooked in a rich and flavorful sauce made with doubanjiang (fermented bean paste) and ground pork.",
+        category: "Chinese"
     }]
 
 export const url = 'http://localhost:4000'
